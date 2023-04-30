bee840ae5ea2e132563752220c5610d50b82e40b
milanswanthra <swanthrawkm.20@uom.lk>
2023-05-01 04:01:26 +0530

    Minor fixes

--- flask/server.py
+++ flask/server.py
@@ -14,59 +14,57 @@ CORS(app)
 model = pickle.load(open('model.pkl', 'rb'))
 
 
-
 def write_to_file(data):
     filename = 'new_players.csv'
     fields = [
-                     "Name",
-                     "Nickname",
-                     "Gender",
-                     "Date_of_birth",
-                     "Weight",
-                     "Height",
-                     "Team",
-                     "Shirt_number",
-                     "Position",
-                     "Aerial_duels_won",
-                     "Appearances",
-                     "Assists_intentional",
-                     "Attempts_from_set_pieces",
-                     "Blocks",
-                     "Catches",
-                     "Clean_sheets",
-                     "Duels_won",
-                     "Goals",
-                     "Goals_conceded",
-                     "Interceptions",
-                     "Key_passes_attempt_assists",
-                     "Offsides",
-                     "Open_play_passes",
-                     "Penalties_saved",
-                     "Red_cards_2nd_yellow",
-                     "Saves_made",
-                     "Shots_on_target_inc_goals",
-                     "Straight_red_cards",
-                     "Successful_crosses_open_play",
-                     "Successful_dribbles",
-                     "Successful_long_passes",
-                     "Successful_passes_opposition_half",
-                     "Successful_passes_own_half",
-                     "Successful_short_passes",
-                     "Total_clearances",
-                     "Total_fouls_conceded",
-                     "Total_losses_of_possession",
-                     "Total_passes",
-                     "Total_tackles",
-                     "Total_touches_in_opposition_box",
-                     "Unsuccessful_dribbles",
-                     "Unsuccessful_long_passes",
-                     "Unsuccessful_short_passes",
-                     "Yellow_cards",
-                     "gender"
-                 ]
+        "Name",
+        "Nickname",
+        "Gender",
+        "Date_of_birth",
+        "Weight",
+        "Height",
+        "Team",
+        "Shirt_number",
+        "Position",
+        "Aerial_duels_won",
+        "Appearances",
+        "Assists_intentional",
+        "Attempts_from_set_pieces",
+        "Blocks",
+        "Catches",
+        "Clean_sheets",
+        "Duels_won",
+        "Goals",
+        "Goals_conceded",
+        "Interceptions",
+        "Key_passes_attempt_assists",
+        "Offsides",
+        "Open_play_passes",
+        "Penalties_saved",
+        "Red_cards_2nd_yellow",
+        "Saves_made",
+        "Shots_on_target_inc_goals",
+        "Straight_red_cards",
+        "Successful_crosses_open_play",
+        "Successful_dribbles",
+        "Successful_long_passes",
+        "Successful_passes_opposition_half",
+        "Successful_passes_own_half",
+        "Successful_short_passes",
+        "Total_clearances",
+        "Total_fouls_conceded",
+        "Total_losses_of_possession",
+        "Total_passes",
+        "Total_tackles",
+        "Total_touches_in_opposition_box",
+        "Unsuccessful_dribbles",
+        "Unsuccessful_long_passes",
+        "Unsuccessful_short_passes",
+        "Yellow_cards",
+        "gender"
+    ]
 
     file_exists = os.path.isfile(filename)
-
 
     try:
         with open(filename, 'a', newline='') as file:
@@ -81,9 +79,9 @@ def write_to_file(data):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_values = [0 if k == "gender" and v.lower() == "male" else 1 if k == "gender" and v.lower() == "female" else float(v) for k, v in request.json.items() if k not in ["Name", "Nickname", "Gender", "Date_of_birth", "Weight", "Height", "Team", "Shirt_number", "Position"]]
+    input_values = [0 if k == "gender" and v.lower() == "male" else 1 if k == "gender" and v.lower() == "female" else float(
+        v) for k, v in request.json.items() if k not in ["Name", "Nickname", "Gender", "Date_of_birth", "Weight", "Height", "Team", "Shirt_number", "Position"]]
     write_to_file(request.json)
-
 
     stat_cols = ['aerial_duels_won', 'appearances', 'assists_intentional', 'attempts_from_set_pieces', 'blocks',
                  'catches', 'clean_sheets', 'duels_won', 'goals', 'goals_conceded', 'interceptions',
@@ -124,7 +122,8 @@ def get_data():
     forwards_file = forwards_file.fillna(value='NaN')
     defenders_file = defenders_file.fillna(value='NaN')
 
-    all_players_df = pd.concat([goalkeepers_file, midfielders_file, forwards_file, defenders_file])
+    all_players_df = pd.concat(
+        [goalkeepers_file, midfielders_file, forwards_file, defenders_file])
 
     goalkeepers_df = all_players_df[all_players_df['predicted_position'] == 'Goalkeeper']
     midfielders_df = all_players_df[all_players_df['predicted_position'] == 'Midfielder']
@@ -142,10 +141,10 @@ def get_data():
     defenders = defenders_df.head(6).to_dict(orient='records')
     return jsonify(
         {'defenders': defenders,
-        'forwards': forwards,
-        'goalkeepers': goalkeepers,
-        'midfielders': midfielders,
-        })
+         'forwards': forwards,
+         'goalkeepers': goalkeepers,
+         'midfielders': midfielders,
+         })
 
 
 @app.route('/altdata')
@@ -160,10 +159,25 @@ def get_altdata():
     forwards_file = forwards_file.fillna(value='NaN')
     defenders_file = defenders_file.fillna(value='NaN')
 
-    goalkeepers_df = goalkeepers_file.sort_values(by='score', ascending=False)
-    midfielders_df = midfielders_file.sort_values(by='score', ascending=False)
-    forwards_df = forwards_file.sort_values(by='score', ascending=False)
-    defenders_df = defenders_file.sort_values(by='score', ascending=False)
+    goalkeepers = pd.DataFrame()
+    midfielders = pd.DataFrame()
+    forwards = pd.DataFrame()
+    defenders = pd.DataFrame()
+
+    for x in [goalkeepers_file, midfielders_file, forwards_file, defenders_file]:
+        goalkeepers = pd.concat(
+            [goalkeepers, x[x['position'] == 'Goalkeeper']])
+        midfielders = pd.concat(
+            [midfielders, x[x["position"] == "Midfielder"]])
+        forwards = pd.concat([forwards, x[x["position"] == "Forward"]])
+        defenders = pd.concat([defenders, x[x["position"] == "Defender"]])
+
+    print(len(goalkeepers))
+
+    goalkeepers_df = goalkeepers.sort_values(by='score', ascending=False)
+    midfielders_df = midfielders.sort_values(by='score', ascending=False)
+    forwards_df = forwards.sort_values(by='score', ascending=False)
+    defenders_df = defenders.sort_values(by='score', ascending=False)
 
     top_goalkeepers = goalkeepers_df.head(6).to_dict(orient='records')
     bottom_goalkeepers = goalkeepers_df.tail(8).to_dict(orient='records')
@@ -179,34 +193,38 @@ def get_altdata():
                 'top': top_defenders,
                 'bottom': bottom_defenders
             },
-        'forwards':
-                {
-                    'top': top_forwards,
-                    'bottom': bottom_forwards
-                },
-        'goalkeepers':
-                {
-                    'top': top_goalkeepers,
-                    'bottom': bottom_goalkeepers
-                },
-        'midfielders':
-                {
-                    'top': top_midfielders,
-                    'bottom': bottom_midfielders
-                }
-        })
+         'forwards':
+         {
+                'top': top_forwards,
+                'bottom': bottom_forwards
+            },
+         'goalkeepers':
+         {
+                'top': top_goalkeepers,
+                'bottom': bottom_goalkeepers
+            },
+         'midfielders':
+         {
+                'top': top_midfielders,
+                'bottom': bottom_midfielders
+            }
+         })
+
 
 @app.route('/comparison')
 def get_comparison():
     players = []
-    files = ['defenders.csv', 'forwards.csv', 'goalkeepers.csv', 'mmidfielders.csv']
+    files = ['defenders.csv', 'forwards.csv',
+             'goalkeepers.csv', 'mmidfielders.csv']
     for file in files:
         df = pd.read_csv(file)
         df = df.fillna(value='NaN')
-        df = df[['name', 'position', 'date_of_birth', 'weight', 'goals', 'height', 'photo', 'team', 'shirt_number']]
+        df = df[['name', 'position', 'date_of_birth', 'weight',
+                 'goals', 'height', 'photo', 'team', 'shirt_number']]
         df = df.to_dict(orient='records')
         players.append(df)
     return jsonify({'players': players})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
